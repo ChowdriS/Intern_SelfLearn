fc78eb6d6de2490679ccd1b178bbe04da0340f7b
Chowdri Sakthivel <114232036+ChowdriS@users.noreply.github.com>
2025-09-01 15:47:26 +0530

    GNU + Linux practice

--- C#_Tasks/DoctorApi/Model/Patient.cs
+++
@@ -1,9 +0,0 @@
-namespace DoctorApi.Models
-{
-    public class Patient
-    {
-        public int Id { get; set; }
-        public string FullName { get; set; } = string.Empty;
-        public DateTime DateOfBirth { get; set; }
-    }
-}
